ab9c6a32c00ddeeac3c26eb49d841c1582abcb89
Vivienne <vklaassens@gmail.com>
2017-04-25 08:25:20 +0200

    Working version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414085110) do
+ActiveRecord::Schema.define(version: 20170424193810) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
